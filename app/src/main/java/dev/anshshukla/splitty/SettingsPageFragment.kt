43058891f15b6c9f82fb4549fc6d6fe157eb9b85
Ansh Shukla <contact@anshshukla.dev>
2022-08-07 17:12:31 -0700

    user settings

--- app/src/main/java/dev/anshshukla/splitty/SettingsPageFragment.kt
+++ app/src/main/java/dev/anshshukla/splitty/SettingsPageFragment.kt
@@ -1,7 +1,14 @@
 package dev.anshshukla.splitty
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class SettingsPageFragment : PreferenceFragmentCompat() {
 
