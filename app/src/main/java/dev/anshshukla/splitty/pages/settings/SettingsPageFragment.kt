a67b2c16142ab3214ad4c77cf885c87a8692644f
Ansh Shukla <contact@anshshukla.dev>
2022-08-08 11:21:29 -0700

    logout button

--- app/src/main/java/dev/anshshukla/splitty/pages/settings/SettingsPageFragment.kt
+++ app/src/main/java/dev/anshshukla/splitty/pages/settings/SettingsPageFragment.kt
@@ -1,20 +1,24 @@
 package dev.anshshukla.splitty.pages.settings
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.bumptech.glide.Glide
+import com.firebase.ui.auth.AuthUI
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
+import dev.anshshukla.splitty.FirebaseUIActivity
 import dev.anshshukla.splitty.R
 import dev.anshshukla.splitty.pages.settings.fragments.UserSettingsDialogFragment
 
@@ -28,14 +32,16 @@ class SettingsPageFragment : Fragment() {
         val view = inflater.inflate(R.layout.page_settings, container, false)
         setupUserSettingsCard(view)
 
-//        AuthUI.getInstance().signOut(this)
-//            .addOnCompleteListener {
-//                run {
-//                    // user is now signed out
-//                    startActivity(Intent(this, FirebaseUIActivity::class.java))
-//                    finish()
-//                }
-//            }
+        view.findViewById<LinearLayout>(R.id.logoutButton).setOnClickListener {
+            context?.let { context ->
+                AuthUI.getInstance().signOut(context)
+                    .addOnCompleteListener {
+                        // user is now signed out
+                        startActivity(Intent(context, FirebaseUIActivity::class.java))
+                    }
+            }
+        }
+
         return view;
     }
 
